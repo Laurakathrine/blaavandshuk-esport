bd41edcb2c5049503c7217c0b28d07298dbb8aa6
cmaweb <semakokten@gmail.com>
2018-05-23 18:01:58 +0200

    worked on header

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+function dropDown(){
+	var content = document.getElementById('myDropdown');
+	var overlay = document.getElementById('dropdown-overlay');
+	content.style.display = 'block';
+	overlay.style.display = 'block';
+
+}
+
+function closeDropdown(){
+var content = document.getElementById('myDropdown');
+	var overlay = document.getElementById('dropdown-overlay');
+	content.style.display = 'none';
+	overlay.style.display = 'none';	
+}
