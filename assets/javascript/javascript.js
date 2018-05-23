fe41b25522fc3a185c02171334628162b7c086ac
cmaweb <semakokten@gmail.com>
2018-05-23 19:57:54 +0200

    changes after validation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,26 @@
 function dropDown(){
 	var content = document.getElementById('myDropdown');
 	var overlay = document.getElementById('dropdown-overlay');
+	var dropbtn = document.getElementById('trigger');
+
 	content.style.display = 'block';
 	overlay.style.display = 'block';
 
+	dropbtn.classList.add('active');
 }
 
 function closeDropdown(){
 var content = document.getElementById('myDropdown');
-	var overlay = document.getElementById('dropdown-overlay');
+var overlay = document.getElementById('dropdown-overlay');
+var dropbtn = document.getElementById('trigger');
+
 	content.style.display = 'none';
 	overlay.style.display = 'none';	
+	dropbtn.classList.remove('active');
 }
+
+function nextSlide() {
+    var q = function(sel) { return document.querySelector(sel); }
+    q(".slides").appendChild(q(".slides img:first-child"));
+}
+setInterval(nextSlide, 3000);
